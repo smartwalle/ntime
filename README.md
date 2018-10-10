32059e9556e5cd8510bb3b65c11bb360019491fb
SmartWalle <smartwalle@gmail.com>
2018-10-10 10:02:38 +0800

    更新 Readme

--- README.md
+++ README.md
@@ -52,6 +52,40 @@ now.EndDateOfMonth()
 time4go.JSONFormatter = time4go.DefaultFormatter{Layout: "2006-01-02 15:04:05"}
 ```
 
+例子:
+
+```
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/smartwalle/time4go"
+)
+
+func main() {
+	time4go.JSONFormatter = time4go.DefaultFormatter{Layout: "2006-01-02 15:04:05"}
+
+	var s = &Schedule{}
+	s.Begin = time4go.Now()
+	s.End = s.Begin.AddDate(0, 1, 0)
+
+	sBytes, _ := json.Marshal(s)
+	fmt.Println(string(sBytes))
+
+	var ts = `{"begin":"2019-11-10 09:59:21","end":"2019-12-10 09:59:21"}`
+	var s2 *Schedule
+	json.Unmarshal([]byte(ts), &s2)
+	fmt.Println(s2.Begin, s2.End)
+}
+
+type Schedule struct {
+	Begin *time4go.Time `json:"begin"`
+	End   *time4go.Time `json:"end"`
+}
+
+```
+
 * 自定义 Formatter
 
 当然你也可以自定义 Formatter，只需要实现以下接口即可：
--- sample/main.go
+++ sample/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/smartwalle/time4go"
-	"time"
 )
 
 func main() {
-	var now = time4go.Now()
-	fmt.Println(now.BeginningDateOfMonth())
-	fmt.Println(now.EndDateOfMonth())
+	time4go.JSONFormatter = time4go.DefaultFormatter{Layout: "2006-01-02 15:04:05"}
 
-	var d = time4go.Date(2018, time.May, 20, 13, 14, 0, 0, time.Local)
-	fmt.Println(d)
+	var s = &Schedule{}
+	s.Begin = time4go.Now()
+	s.End = s.Begin.AddDate(0, 1, 0)
+
+	sBytes, _ := json.Marshal(s)
+	fmt.Println(string(sBytes))
+
+	var ts = `{"begin":"2019-11-10 09:59:21","end":"2019-12-10 09:59:21"}`
+	var s2 *Schedule
+	json.Unmarshal([]byte(ts), &s2)
+	fmt.Println(s2.Begin, s2.End)
+
+	//var now = time4go.Now()
+	//fmt.Println(now.BeginningDateOfMonth())
+	//fmt.Println(now.EndDateOfMonth())
+}
+
+type Schedule struct {
+	Begin *time4go.Time `json:"begin"`
+	End   *time4go.Time `json:"end"`
 }
