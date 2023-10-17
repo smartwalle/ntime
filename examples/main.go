39e945a5724a3bcd033ce6f6fa0cfeefc81636ce
smartwalle <smartwalle@gmail.com>
2023-10-17 13:27:17 +0800

    重构

--- examples/main.go
+++ examples/main.go
@@ -11,20 +11,20 @@ func main() {
 	ntime.JSONFormatter = ntime.DefaultFormatter{Layout: "2006-01-02 15:04:05"}
 
 	var s = &Schedule{}
-	s.Begin = ntime.Now()
-	//s.End = s.Start.AddDate(0, 1, 0)
+	s.Start = ntime.Now()
+	s.End = s.Start.AddDate(0, 1, 0)
 
 	sBytes, _ := json.Marshal(s)
 	fmt.Println(string(sBytes))
 
-	var ts = `{"begin":"2019-11-10 09:59:21","end":"2019-12-10 09:59:21"}`
+	var ts = `{"start":"2019-11-10 09:59:21","end":"2019-12-10 09:59:21"}`
 	var s2 *Schedule
 	json.Unmarshal([]byte(ts), &s2)
-	fmt.Println(s2.Begin, s2.End)
+	fmt.Println(s2.Start, s2.End)
 
-	//var now = ntime.Now()
-	//fmt.Println(now.BeginningDateOfMonth())
-	//fmt.Println(now.EndDateOfMonth())
+	var now = ntime.Now()
+	fmt.Println(now.FirstDayOfMonth().Start())
+	fmt.Println(now.LastDayOfMonth().End())
 
 	var t = ntime.Date(2019, time.January, 1, 0, 0, 0, 0, ntime.UTC)
 
@@ -32,6 +32,6 @@ func main() {
 }
 
 type Schedule struct {
-	Begin ntime.Time `json:"begin"`
+	Start ntime.Time `json:"start"`
 	End   ntime.Time `json:"end"`
 }
